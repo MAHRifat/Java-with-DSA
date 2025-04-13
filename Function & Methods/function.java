80be75e61818ed5ad4ebf5ae95b0462afaeae6a2
MAHRifat <hasanrifata2@gmail.com>
2025-04-13 12:39:00 +0600

    Function basic

--- Function & Methods/function.java
+++ Function & Methods/function.java
@@ -2,8 +2,25 @@ public class function {
     public static int  abc(int a, int b){   // parameters / formal parameters
         return a+b;
     }
+
+    public static int factorial(int a){
+        if(a ==0 || a == 1){
+            return 0;
+        }
+        int res=1;
+        for(int i=1;i<=a;i++){
+            res = res*i;
+        }
+        return res;
+    }
+
+    public static int binomialCoefficient(int n, int r){
+        return (factorial(n)/(factorial(r)*factorial(n-r)));
+    }
+
     public static void main(String[] args) {
         System.out.println(abc(3,4));    // arguments / actual parameters
+        System.out.println(binomialCoefficient(4, 2));
     } 
 }
  
@@ -12,3 +29,4 @@ public class function {
 
 // java not work by call by referance  ->>>>> original copy pass to the function
 
+// function overloading parameter depandent...... not returntype
